86ae77e72244714af779b3243ec9d384902bf0c4
michael osondu anudu <michaelanudu@gmail.com>
2023-03-17 18:34:05 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
-
 /**
- * main - main function
+ * main - main function of data types in C
  *
- * Return: 0 on success
- */
-
+ * Return: 0
+*/
 int main(void)
 {
-	printf("Size of a char: %lu byte(s)", sizeof(char));
-	printf("Size of an int: %lu byte(s)", sizeof(int));
-	printf("Size of a long int: %lu byte(s)", sizeof(long int));
-	printf("Size of a long long int: %lu byte(s)", sizeof(long long int));
-	printf("Size of a float: %lu byte(s)", sizeof(float));
+	char a;
+	int b;
+	long int c;
+	long int d;
+	float f;
+
+	printf("Size of a char: %lu byte(S)\n", (unsigned long)sizeof(a));
+	printf("Size of an int: %lu byte(S)\n", (unsigned long)sizeof(b));
+	printf("Size of a long int: %lu byte(S)\n", (unsigned long)sizeof(c));
+	printf("Size of a long long int: %lu byte(S)\n", (unsigned long)sizeof(d));
+	printf("Size of a float: %lu byte(S)\n", (unsigned long)sizeof(f));
 	return (0);
 }
