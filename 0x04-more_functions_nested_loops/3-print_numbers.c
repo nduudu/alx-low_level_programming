b15bfa487c9daa37bc7852c100a2b4c1140d00dc
michael osondu anudu <michaelanudu@gmail.com>
2023-03-25 09:30:17 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * print_numbers - print numbers from 0 to 9
- * Return: void
+ * print_numbers -  print numbers from 0 to 9
+ * Return: Always 0
  */
-
 void print_numbers(void)
 {
 	char c;
