d4ce1aed933ec78f06fa79ccd8c52716f0c12642
michael osondu anudu <michaelanudu@gmail.com>
2023-03-27 09:37:59 -0700

    no _putchar

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,17 @@
-#include "main.h"
+#include "stdio.h"
 
 /**
  * print_numbers -  print numbers from 0 to 9
- * Return: Always 0
+ * Return: void
  */
+
 void print_numbers(void)
 {
-	char c;
+	int i;
 
-	for (c = '0'; c <= '9'; c++)
+	for (i = 0; i < 10; i++)
 	{
-		_putchar(c);
+		putchar(i + '0');
 	}
-	_putchar('\n');
+	putchar('\n');
 }
