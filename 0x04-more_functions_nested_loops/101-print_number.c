423e7a4a893a37a459120eb0930eaea6f87596d8
Anudu Michael Osondu <michaelanudu@gmail.com>
2023-05-02 12:18:14 -0700

    print_listint

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,23 +2,25 @@
 
 /**
  * print_number - prints an integer
- * @n: input integer parameter
- *
- * Return: Always 0
+ * @n: integer to be printed
  */
-
 void print_number(int n)
 {
-	unsigned int i = n;
+	unsigned int n1;
 
 	if (n < 0)
 	{
-		putchar(45);
-		i = -i;
-	}
-	if (i / 10)
+		n1 = -n;
+		putchar('-');
+	} else
 	{
-		print_number(i / 10);
+		n1 = n;
 	}
-	putchar(i % 10 + '0');
+
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
+	}
+
+	putchar((n1 % 10) + '0');
 }
